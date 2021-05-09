7419370026647d1ef24f208a6abe21e3872943bc
engrCoy <83744411+engrCoy@users.noreply.github.com>
2021-05-09 13:29:39 +0800

    more practice
    jS, and css practice codes

---
+++ javascript_practice_7.js
@@ -0,0 +1,17 @@
+let lex = function (input, output) {
+    let lexArr = [];
+    let vowels = ['a','e','i','o','u'];
+    let ctr = 0;
+    for(let i = 0; i < output; i++){
+        for(let j = i; j < vowels.length; j++){
+            //while()
+        }
+
+    }
+    return lexArr;
+}
+
+console.log(lex(2,15));
+console.log(lex(3,5));
+
+// dynamic programming.....brb
